9b001e6e56dc748bc0c6b45c0c7987fe5932d886
ariel_grubbs <arielvictor1234@gmail.com>
2020-11-16 18:29:26 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(list){
+    let arr1 = []
+    for (let i = 0; i < list.length; i += 1){
+        arr1.push(`Thank you, ${list[i]}, for the wonderful surprise gift!`)
+    }
+    return arr1
+}
+
+function countDown(num) {
+    while (num >= 0) {
+      console.log(num);
+      num -= 1;
+    }
+  }
